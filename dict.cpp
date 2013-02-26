c8e5c0515a304ae5a939768481c83b900540ed37
Nathan Lin <natlin@ucdavis.edu>
2013-02-26 00:21:55 -0800

    Changed the sentence reading loop to delete \n from a while to a do while

--- dict.cpp
+++ dict.cpp
@@ -38,14 +38,21 @@ Dict::Dict(string f)
     while(getline(fin, str, '.'))
     {
       sentence.push_back(str);
-      while((enter = sentence[j].find('\n'))
-            && enter != static_cast<int>(string::npos))
-        sentence[j].replace(enter, 1, " ");
+      //while((enter = sentence[j].find('\n'))
+      //      && enter != static_cast<int>(string::npos))
+      do
+      {
+        enter = sentence[j].find('\n');
+        if(enter != static_cast<int>(string::npos))
+          sentence[j].replace(enter, 1, " ");
+      }while (enter != static_cast<int>(string::npos));//while
       if(sentence[j][0] == ' ')
         sentence[j].replace(0, 1, "");
       cout << sentence[j] << endl;
       j++;
     }//while
+      cout << "blah" << endl;
+      cout << sentence[9] << endl;
   }//if
   else
   {
